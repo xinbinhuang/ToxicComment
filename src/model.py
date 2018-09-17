5880582d291e3bb064b03660c2e3aebd821cfb48
Xinbin Huang <xbhuang93@hotmail.com>
2018-09-17 01:02:10 -0700

    Update model

--- src/model.py
+++ src/model.py
@@ -8,36 +8,39 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 
 # load data
-train_df=pd.read_csv('../data/train.csv')
-test_df=pd.read_csv('../data/test.csv')
+train_df = pd.read_csv('../data/train.csv')
+test_df = pd.read_csv('../data/test.csv')
+
+class_names = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
 
 # combine text for preprocessing
-train_text=train_df["comment_text"]
-test_text=test_df["comment_text"]
-all_text=pd.concat([train_text, test_text])
+train_text = train_df["comment_text"]
+test_text = test_df["comment_text"]
+all_text = pd.concat([train_text, test_text])
 
 # transform comment into tfidf features
-tfidf_vectorizer=CountVectorizer(encoding="unicode", 
-                analyzer="word",
-                token_pattern="\w{1,}",
-                stop_words="english",
-                ngram_range=(1,1),
-                max_features=10000)
+tfidf_vectorizer = TfidfVectorizer(encoding="unicode",
+                                   analyzer="word",
+                                   token_pattern="\w{1,}",
+                                   stop_words="english",
+                                   ngram_range=(1, 1),
+                                   max_features=10000)
 tfidf_vectorizer.fit(all_text)
-train_tfidf_features=tfidf_vectorizer.transform(train_text)
-test_tfidf_features=tfidf_vectorizer.transform(test_text)
+train_features = tfidf_vectorizer.transform(train_text)
+test_features = tfidf_vectorizer.transform(test_text)
 
 # train the model
-scores=[]
-y_pred_tfidf=pd.DataFrame.from_dict({id: test_df["id"]})
+scores = []
+y_pred_tfidf = pd.DataFrame.from_dict({id: test_df["id"]})
 for class_name in class_names:
     train_target = train_df[class_name]
     classifier = LogisticRegression(solver="sag")
-    
-    cv_score = np.mean(cross_val_score(classifier, train_tfidf_features, train_target, cv=3, scoring="roc_auc"))
+
+    cv_score = np.mean(cross_val_score(classifier, train_features,
+                                       train_target, cv=3, scoring="roc_auc"))
     scores.append(cv_score)
     print("Average CV score for {} : {}".format(class_name, cv_score))
-    
-    classifier.fit(train_tfidf_features, train_target)
-    y_pred_tfidf[class_name]=classifier.predict_proba(test_tfidf_features)[:, 1]
+
+    classifier.fit(train_features, train_target)
+    y_pred_tfidf[class_name] = classifier.predict_proba(test_features)[:, 1]
 print("Total average CV score for all classes : {}".format(np.mean(scores)))
